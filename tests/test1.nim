668e1efa135e7f7a9bd9aa80e84fa48111421bd7
harungo <1reckoner1@gmail.com>
2019-02-10 16:50:48 +0300

    Update test1.nim

--- tests/test1.nim
+++ tests/test1.nim
@@ -6,3 +6,11 @@ test "1d Noise test":
   var noise = noise(0.123) # Get the noise value for the coordinate
   echo noise
 
+test "2d Noise test":
+  var noise = noise(0.123, 0.123) # Get the noise value for the coordinate
+  echo noise
+
+
+test "3d Noise test":
+  var noise = noise(0.123, 0.123, 0.123) # Get the noise value for the coordinate
+  echo noise
